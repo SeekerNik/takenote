ac6b924b8897cc4e773356c968a2ce4e43e13386
nik6798 <nikhilsaini94169@gmail.com>
2020-07-10 11:39:08 +0530

    Updates

---
+++ precache-manifest.5379f2b15e14335f691d037c11cbec40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f8a4f4bee5ed4f3b5006f0f7b36cb5",
+    "url": "/takenote/index.html"
+  },
+  {
+    "revision": "c41e2285581883518855",
+    "url": "/takenote/static/js/2.38077634.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/takenote/static/js/2.38077634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1ebb1207fa6c24972f",
+    "url": "/takenote/static/js/main.cda9d011.chunk.js"
+  },
+  {
+    "revision": "01fcf73d0bfb3a76f975",
+    "url": "/takenote/static/js/runtime-main.fe2f9c93.js"
+  }
+]);
